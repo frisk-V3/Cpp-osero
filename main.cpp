2de39a4cf570ab8c1ba2b964164d74d4f4509030
frisk-V3 <260639938+frisk-V3@users.noreply.github.com>
2026-04-02 13:35:18 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,158 +1,178 @@
-#include <iostream>
+#ifndef UNICODE
+#define UNICODE
+#endif
+
+#include <windows.h>
 #include <vector>
 #include <string>
-#include <algorithm>
 #include <fstream>
 #include <map>
 
-using namespace std;
-
-enum Space { EMPTY, BLACK, WHITE };
+// --- 定数定義 ---
+const int CELL_SIZE = 60;
+const int BOARD_MARGIN = 50;
 const int SIZE = 8;
-const string SAVE_FILE = "player_data.txt";
+const wchar_t* DATA_PATH = L"brain.dat";
 
-struct Config {
-    int mode; // 1: 1P, 2: 2P
-    int cpu_level; // 1: Easy, 2: Normal, 3: Hard
-    string black_color = "\033[32m"; // Default Green (for board)
-    string stone_b = "B";
-    string stone_w = "W";
-};
+enum class Space { EMPTY, BLACK, WHITE };
 
-class Othello {
-    Space board[SIZE][SIZE];
-    map<string, int> player_habits; // 座標ごとの統計
+// --- グローバル変数 ---
+Space board[SIZE][SIZE];
+Space current_turn = Space::BLACK;
+int cpu_level = 2; // 1:Easy, 2:Normal, 3:Hard
+bool vs_cpu = true;
+std::map<int, int> player_habits;
 
-public:
-    Othello() {
-        for(int i=0; i<SIZE; ++i) for(int j=0; j<SIZE; ++j) board[i][j] = EMPTY;
-        board[3][3] = WHITE; board[4][4] = WHITE;
-        board[3][4] = BLACK; board[4][3] = BLACK;
-        load_learning_data();
+// --- ロジック関数 ---
+void SaveHabit(int r, int c) {
+    player_habits[r * 10 + c]++;
+    std::ofstream ofs(DATA_PATH, std::ios::binary);
+    for (auto const& [pos, count] : player_habits) {
+        ofs.write((char*)&pos, sizeof(pos));
+        ofs.write((char*)&count, sizeof(count));
     }
+}
 
-    void load_learning_data() {
-        ifstream ifs(SAVE_FILE);
-        string pos; int count;
-        while(ifs >> pos >> count) player_habits[pos] = count;
+void LoadHabit() {
+    std::ifstream ifs(DATA_PATH, std::ios::binary);
+    int pos, count;
+    while (ifs.read((char*)&pos, sizeof(pos)) && ifs.read((char*)&count, sizeof(count))) {
+        player_habits[pos] = count;
     }
+}
 
-    void save_learning_data(int r, int c) {
-        string pos = to_string(r) + to_string(c);
-        player_habits[pos]++;
-        ofstream ofs(SAVE_FILE);
-        for(auto const& [key, val] : player_habits) ofs << key << " " << val << endl;
-    }
-
-    void display(Config& conf) {
-        cout << "\n  0 1 2 3 4 5 6 7" << endl;
-        for(int i=0; i<SIZE; ++i) {
-            cout << i << " ";
-            for(int j=0; j<SIZE; ++j) {
-                if(board[i][j] == EMPTY) cout << ". ";
-                else if(board[i][j] == BLACK) cout << conf.stone_b << " ";
-                else cout << conf.stone_w << " ";
+bool IsValid(int r, int c, Space s) {
+    if (r < 0 || r >= SIZE || c < 0 || c >= SIZE || board[r][c] != Space::EMPTY) return false;
+    for (int dr = -1; dr <= 1; ++dr) {
+        for (int dc = -1; dc <= 1; ++dc) {
+            if (dr == 0 && dc == 0) continue;
+            int nr = r + dr, nc = c + dc;
+            bool found_opp = false;
+            while (nr >= 0 && nr < SIZE && nc >= 0 && nc < SIZE && board[nr][nc] == (s == Space::BLACK ? Space::WHITE : Space::BLACK)) {
+                nr += dr; nc += dc; found_opp = true;
             }
-            cout << endl;
+            if (found_opp && nr >= 0 && nr < SIZE && nc >= 0 && nc < SIZE && board[nr][nc] == s) return true;
         }
     }
+    return false;
+}
 
-    bool is_valid(int r, int c, Space s) {
-        if(board[r][c] != EMPTY) return false;
-        for(int dr=-1; dr<=1; ++dr) {
-            for(int dc=-1; dc<=1; ++dc) {
-                if(dr==0 && dc==0) continue;
-                int nr = r + dr, nc = c + dc;
-                bool found_opp = false;
-                while(nr>=0 && nr<SIZE && nc>=0 && nc<SIZE && board[nr][nc] == (s == BLACK ? WHITE : BLACK)) {
-                    nr += dr; nc += dc; found_opp = true;
-                }
-                if(found_opp && nr>=0 && nr<SIZE && nc>=0 && nc<SIZE && board[nr][nc] == s) return true;
+void Flip(int r, int c, Space s) {
+    board[r][c] = s;
+    for (int dr = -1; dr <= 1; ++dr) {
+        for (int dc = -1; dc <= 1; ++dc) {
+            if (dr == 0 && dc == 0) continue;
+            int nr = r + dr, nc = c + dc;
+            std::vector<std::pair<int, int>> targets;
+            while (nr >= 0 && nr < SIZE && nc >= 0 && nc < SIZE && board[nr][nc] == (s == Space::BLACK ? Space::WHITE : Space::BLACK)) {
+                targets.push_back({nr, nc});
+                nr += dr; nc += dc;
             }
-        }
-        return false;
-    }
-
-    void place(int r, int c, Space s) {
-        board[r][c] = s;
-        for(int dr=-1; dr<=1; ++dr) {
-            for(int dc=-1; dc<=1; ++dc) {
-                if(dr==0 && dc==0) continue;
-                int nr = r + dr, nc = c + dc;
-                vector<pair<int,int>> to_flip;
-                while(nr>=0 && nr<SIZE && nc>=0 && nc<SIZE && board[nr][nc] == (s == BLACK ? WHITE : BLACK)) {
-                    to_flip.push_back({nr, nc});
-                    nr += dr; nc += dc;
-                }
-                if(nr>=0 && nr<SIZE && nc>=0 && nc<SIZE && board[nr][nc] == s) {
-                    for(auto p : to_flip) board[p.first][p.second] = s;
-                }
+            if (!targets.empty() && nr >= 0 && nr < SIZE && nc >= 0 && nc < SIZE && board[nr][nc] == s) {
+                for (auto& p : targets) board[p.first][p.second] = s;
             }
         }
     }
+}
 
-    pair<int,int> get_cpu_move(int level, Space s) {
-        vector<pair<int,int>> moves;
-        for(int i=0; i<SIZE; ++i) for(int j=0; j<SIZE; ++j) if(is_valid(i,j,s)) moves.push_back({i,j});
-        
-        if(level == 1) return moves[rand() % moves.size()];
+// --- GUI描画 ---
+void DrawBoard(HDC hdc) {
+    // 盤面（緑）
+    HBRUSH hBrush = CreateSolidBrush(RGB(34, 139, 34));
+    SelectObject(hdc, hBrush);
+    Rectangle(hdc, BOARD_MARGIN, BOARD_MARGIN, BOARD_MARGIN + SIZE * CELL_SIZE, BOARD_MARGIN + SIZE * CELL_SIZE);
+    DeleteObject(hBrush);
 
-        // Normal/Hard: 角を狙う or プレイヤーの癖(頻出座標)をブロック
-        sort(moves.begin(), moves.end(), [&](pair<int,int> a, pair<int,int> b){
-            int score_a = 0, score_b = 0;
-            auto eval = [&](int r, int c) {
-                int res = 0;
-                if((r==0||r==7) && (c==0||c==7)) res += 100; // 角
-                string pos = to_string(r) + to_string(c);
-                if(level == 3) res += player_habits[pos] * 10; // 学習効果: 相手が好む場所を優先奪取
-                return res;
-            };
-            return eval(a.first, a.second) > eval(b.first, b.second);
-        });
-        return moves[0];
+    for (int i = 0; i <= SIZE; ++i) {
+        MoveToEx(hdc, BOARD_MARGIN + i * CELL_SIZE, BOARD_MARGIN, NULL);
+        LineTo(hdc, BOARD_MARGIN + i * CELL_SIZE, BOARD_MARGIN + SIZE * CELL_SIZE);
+        MoveToEx(hdc, BOARD_MARGIN, BOARD_MARGIN + i * CELL_SIZE, NULL);
+        LineTo(hdc, BOARD_MARGIN + SIZE * CELL_SIZE, BOARD_MARGIN + i * CELL_SIZE);
     }
 
-    bool can_move(Space s) {
-        for(int i=0; i<SIZE; ++i) for(int j=0; j<SIZE; ++j) if(is_valid(i,j,s)) return true;
-        return false;
-    }
-};
-
-int main() {
-    Config conf;
-    cout << "=== OTHELLO C++ EDITION ===" << endl;
-    cout << "1: 1 Player (vs CPU)\n2: 2 Players\nSelect: "; cin >> conf.mode;
-    if(conf.mode == 1) {
-        cout << "CPU Level (1:Easy, 2:Normal, 3:Hard): "; cin >> conf.cpu_level;
-    }
-    cout << "Enter Black Stone Char (e.g. B): "; cin >> conf.stone_b;
-    cout << "Enter White Stone Char (e.g. W): "; cin >> conf.stone_w;
-
-    Othello game;
-    Space current = BLACK;
-    while(game.can_move(BLACK) || game.can_move(WHITE)) {
-        game.display(conf);
-        if(!game.can_move(current)) {
-            cout << "No moves! Skip." << endl;
-            current = (current == BLACK ? WHITE : BLACK);
-            continue;
+    for (int r = 0; r < SIZE; ++r) {
+        for (int c = 0; c < SIZE; ++c) {
+            if (board[r][c] != Space::EMPTY) {
+                HBRUSH sBrush = CreateSolidBrush(board[r][c] == Space::BLACK ? RGB(0, 0, 0) : RGB(255, 255, 255));
+                SelectObject(hdc, sBrush);
+                Ellipse(hdc, BOARD_MARGIN + c * CELL_SIZE + 5, BOARD_MARGIN + r * CELL_SIZE + 5,
+                        BOARD_MARGIN + (c + 1) * CELL_SIZE - 5, BOARD_MARGIN + (r + 1) * CELL_SIZE - 5);
+                DeleteObject(sBrush);
+            }
         }
-
-        int r, c;
-        if(conf.mode == 1 && current == WHITE) {
-            pair<int,int> move = game.get_cpu_move(conf.cpu_level, WHITE);
-            r = move.first; c = move.second;
-            cout << "CPU placed at " << r << " " << c << endl;
-        } else {
-            cout << (current == BLACK ? "Black" : "White") << " turn (row col): ";
-            cin >> r >> c;
-            if(!game.is_valid(r,c,current)) { cout << "Invalid!" << endl; continue; }
-            if(current == BLACK) game.save_learning_data(r, c); // プレイヤーの癖を学習
-        }
-        game.place(r, c, current);
-        current = (current == BLACK ? WHITE : BLACK);
     }
-    game.display(conf);
-    cout << "Game Over!" << endl;
+}
+
+// --- ウィンドウプロシージャ ---
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg) {
+    case WM_CREATE:
+        board[3][3] = Space::WHITE; board[4][4] = Space::WHITE;
+        board[3][4] = Space::BLACK; board[4][3] = Space::BLACK;
+        LoadHabit();
+        return 0;
+    case WM_LBUTTONDOWN: {
+        int x = LOWORD(lParam), y = HIWORD(lParam);
+        int c = (x - BOARD_MARGIN) / CELL_SIZE;
+        int r = (y - BOARD_MARGIN) / CELL_SIZE;
+
+        if (IsValid(r, c, current_turn)) {
+            Flip(r, c, current_turn);
+            if (current_turn == Space::BLACK) SaveHabit(r, c);
+            current_turn = (current_turn == Space::BLACK ? Space::WHITE : Space::BLACK);
+            InvalidateRect(hwnd, NULL, TRUE);
+            
+            // CPUの手番（簡易実装）
+            if (vs_cpu && current_turn == Space::WHITE) {
+                PostMessage(hwnd, WM_USER + 1, 0, 0);
+            }
+        }
+        return 0;
+    }
+    case WM_USER + 1: { // CPU思考
+        for(int r=0; r<SIZE; ++r) for(int c=0; c<SIZE; ++c) {
+            if (IsValid(r, c, Space::WHITE)) {
+                Flip(r, c, Space::WHITE);
+                current_turn = Space::BLACK;
+                InvalidateRect(hwnd, NULL, TRUE);
+                return 0;
+            }
+        }
+        current_turn = Space::BLACK;
+        return 0;
+    }
+    case WM_PAINT: {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hwnd, &ps);
+        DrawBoard(hdc);
+        EndPaint(hwnd, &ps);
+        return 0;
+    }
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    }
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nCmdShow) {
+    const wchar_t CLASS_NAME[] = L"OthelloWindowClass";
+    WNDCLASS wc = {};
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    RegisterClass(&wc);
+
+    HWND hwnd = CreateWindowEx(0, CLASS_NAME, L"Elite Othello GUI", WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, 600, 600, NULL, NULL, hInstance, NULL);
+
+    if (hwnd == NULL) return 0;
+    ShowWindow(hwnd, nCmdShow);
+    MSG msg = {};
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
     return 0;
 }
